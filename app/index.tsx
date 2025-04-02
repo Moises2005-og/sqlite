8d0da52e65a9b771a9d4a05db8c25fded1374915
unknown <moises.sebastiao@baxtterangola.com>
2025-04-02 16:01:02 +0200

    feat: create product

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,59 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Input } from "@/components/input";
+import { useProductDatabase } from "@/database/useProductDatabase";
+import { useState } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Home() {
+
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [quantity, setQuantity] = useState("")
+    const databaseProduct = useProductDatabase()
+
+    const handleCreate = async() => {
+        try {
+            const response = await databaseProduct.create({
+                name,
+                quantity: Number(quantity)
+            })
+            alert("Produto cadastrado com o ID: " + response?.insertedRowId)
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+
+    const list = async () => {
+        try {
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
     return(
         <View style={styles.container}>
-            <Text>Home</Text>
+            <View style={{
+                flexDirection: "column",
+                gap: 10,
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Input placeholder="Nome" onChangeText={setName} value={name}/>
+                <Input placeholder="Quantidade" onChangeText={setQuantity} value={quantity}/>
+            </View>
+            <TouchableOpacity activeOpacity={0.8} style={{
+                backgroundColor: "#054f77",
+                width: "80%",
+                padding: 13,
+                borderRadius: 7,
+                marginTop: 10
+            }} onPress={handleCreate}>
+                <Text style={{
+                    color: "#FFF",
+                    textAlign: "center",
+                }}>Cadastrar</Text>
+            </TouchableOpacity>
         </View>
     )
 } 
---
+++ database/useProductDatabase.ts
@@ -0,0 +1,41 @@
+import { useSQLiteContext } from "expo-sqlite"
+
+export type Product = {
+    id: number,
+    name: string,
+    quantity: number
+}
+
+export function useProductDatabase() {
+    
+    const database = useSQLiteContext()
+    const create = async(data: Omit<Product, "id">) => {
+        const statement = await  database.prepareAsync("INSERT INTO products (name, quantity) VALUES ($name, $quantity)")
+
+        try {
+            const result = await statement.executeAsync({
+                $name: data.name,
+                $quantity: data.quantity
+            })
+
+            const insertedRowId = result.lastInsertRowId.toLocaleString()
+
+            return {insertedRowId}
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const searchByName = async (name: string) => {
+        try {
+            const query = "SELECT * FROM products WHERE name LIKE?"
+            const response = await database.getAllAsync(query, `%${name}%`)
+
+            return response
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    return {create}
+}
